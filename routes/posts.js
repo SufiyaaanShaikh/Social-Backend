4dc5d243cadeb8e459c536dfb510bf916a66788b
SufiyaaanShaikh <sufisk2526@gmail.com>
2025-05-19 17:40:59 +0530

    first commit

---
+++ routes/posts.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import {getFeedPosts, getUserPosts, likePost} from '../controllers/posts.js';
+import { verifyToken } from '../middleware/auth.js';    
+
+const router = express.Router();
+
+router.get('/', verifyToken, getFeedPosts); // Get all posts
+router.get('/:userId/posts', verifyToken, getUserPosts); // Get posts by user ID
+
+router.patch('/:id/like', verifyToken, likePost); // Like a post
+
+export default router;
